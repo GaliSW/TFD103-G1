4bf03359fa788caf51b1fc0df28811fd9f699796
Calista <rilakkuma840503@yahoo.com.tw>
2021-11-18 13:58:33 +0800

    add gachaScreen

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -280,3 +280,11 @@ $(function (){
         resizeWindow();
     });
 })
+
+$(document).ready(function() {
+    $('.upup').click(function(event) {
+      event.preventDefault();
+      
+      $('html, body').animate({scrollTop: 0}, 100);
+    })
+  });
