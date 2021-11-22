264eb8ff7c87e3699eb58200d111ebf19bbe1b48
mark83823 <mark83823@gmail.com>
2021-11-22 21:57:31 +0800

    no message

--- src/php/product/insertByCheck.php
+++ src/php/product/insertByCheck.php
@@ -1,13 +1,15 @@
 <?php
-$FK_USERNAME_BUY = $_POST["FK_USERNAME_BUY"];
+// $FK_USERNAME_BUY = $_POST["FK_USERNAME_BUY"];
 $FK_PRODUCT_ID = $_POST["FK_PRODUCT_ID"];
 $FK_GACHA_ID_BUY = $_POST["FK_GACHA_ID_BUY"];
-$CHANGEDATE = $_POST["CHANGEDATE"];
+
 include "../connection.php";  
+include "../Manager.php";
+
 //ho建立SQL 
 
         $sql = "INSERT INTO BYCHECK (FK_USERNAME_BUY, FK_PRODUCT_ID, FK_GACHA_ID_BUY, CHANGEDATE)
-                VALUES(,NOW())";
+                VALUES(?, ?, ?, NOW())";
 
 
     //   $sql = "INSERT INTO PRODUCT(FK_USERNAME, PRICE, TRANS, AMOUNT, ISSHOW, FK_GACHA_ID) VALUES (?, ?, ?, 1, 1, ?);
@@ -15,10 +17,9 @@ include "../connection.php";
        //執行
 
     $statement = $pdo->prepare($sql);
-    $statement->bindValue(1 , $FK_USERNAME_BUY);  
+    $statement->bindValue(1 , getSession());  
     $statement->bindValue(2 , $FK_PRODUCT_ID);
     $statement->bindValue(3 , $FK_GACHA_ID_BUY);
-    $statement->bindValue(4, $CHANGEDATE);  
 
     $statement->execute();
     echo 'y';
