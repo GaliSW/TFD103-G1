7793ad301ff292a1873ecababd7a9a3fed51d7fe
LillianChueh <yvone5116@gmail.com>
2021-11-22 10:52:22 +0800

    collection

--- src/js/member/collection.js
+++ src/js/member/collection.js
@@ -11,25 +11,11 @@ let vmm = new Vue({
         ROLE:[],
     },
     methods: {
-        clickPop() {
-        vmm.$data.pop = true;
-        vmm.$data.buy = true;
-        vmm.$data.success = false;
-        vmm.$data.one = false;
-        vmm.$data.two = false;
-        vmm.$data.three = false;
-        },
+        
         clickClose() {
         vmm.$data.pop = false;
         },
-        clickChange() {
-        vmm.$data.pop = true;
-        vmm.$data.buy = false;
-        vmm.$data.success = false;
-        vmm.$data.one = true;
-        vmm.$data.two = false;
-        vmm.$data.three = false;
-        },
+        
         toSuccess() {
         vmm.$data.success = true;
         vmm.$data.buy = false;
@@ -66,9 +52,11 @@ function display() {
                         let ab5 = rowA.ABILITY.slice(4, 5);
                 if (rowA.AMOUNT == 1) {
                         $("#result").append(
-                        `
+                          `
                             <div class="contain_list">
-                                <div class="role col-2"><img src="../image/ROLE/${rowA.ROLE_IMG}"></div>
+                                <div class="role col-2"><img src="../image/ROLE/${
+                                  rowA.ROLE_IMG
+                                }"></div>
                                 <span class="role_name col-2">
                                     ${rowA.RNAME}
                                     <p class="rwd_price">${rowA.PRICE}E</p>
@@ -88,18 +76,21 @@ function display() {
                                 </div>
                                 <span class="price col-2">${rowA.PRICE}</span>
                                 <div class="trade_button col-2">
-                                    <button class="buy" @click=clickPop()>購買</button>
-                                    <button class="change " @click=clickChange()>交換</button>
+                                    <button class="buy" onclick="clickPop()">購買</button>
+                                    <button class="change " onclick=clickChange()>交換</button>
                                 </div>
                                 <div class="delete col-1">
-                                    <button class="delete_btn" onclick= "deleted(${rowA.FAV_ID})"><i class="fas fa-trash-alt"></i></button>
+                                    <button class="delete_btn" onclick= "deleted(${
+                                      rowA.FAV_ID
+                                    })"><i class="fas fa-trash-alt"></i></button>
                                 </div>
                             </div>
                 
                             <span>
                                 <hr>
                             </span>
-                        `);
+                        `
+                        );
                 }else{
                     $("#result_off").append(
                     `
@@ -161,60 +152,21 @@ function deleted(clickId) {
     });
   }
 
-// 能力圖迴圈
-// let chartLen = document.querySelectorAll(".myChart").length;
-// let chartArr = [];
-// for (let i = 0; i < chartLen; i++) {
-//   let chart = document.querySelectorAll(".myChart")[i].getContext("2d");
 
-//   const data = {
-//     labels: ["加速", "抓地", "碰撞", "迴避", "道具"],
-//     datasets: [
-//       {
-//         label: "",
-//         data: [5, 9, 6, 5, 6],
-//         fill: true,
-//         fillStyle: "red",
-//         backgroundColor: " rgba(16, 197, 177, .5)",
-//         borderColor: "rgba(16, 197, 177, 1)",
-//         pointRadius: 2,
-//         pointBackgroundColor: "rgba(14, 52, 97, .8)",
-//         pointBorderColor: "rgba(2, 79, 63, 1)",
-//         pointHoverRadius: 2,
-//         pointHoverBackgroundColor: "rgba(16, 197, 177, .5)",
-//         pointHoverBorderColor: "rgba(14, 52, 97, 1)",
-//       },
-//     ],
-//   };
-//     const myChart = new Chart(chart, {
-//         type: "radar",
-//         data: data,
-//         options: {
-//             plugins: {
-//                 legend: {
-//                 display: false,
-//                 },
-//             },
-//             scales: {
-//                 r: {
-//                 pointLabels: {
-//                     font: {
-//                     size: 14,
-//                     },
-//                 },
-//                 angleLines: {
-//                     display: true,
-//                 },
-//                 suggestedMin: 0,
-//                 suggestedMax: 10,
-//                 },
-//             },
-//             elements: {
-//                 line: {
-//                 borderWidth: 1,
-//                 },
-//             },
-//         },
-//     });
-// }
+function clickPop() {
+        vmm.$data.pop = true;
+        vmm.$data.buy = true;
+        vmm.$data.success = false;
+        vmm.$data.one = false;
+        vmm.$data.two = false;
+        vmm.$data.three = false;
+        }
 
+function clickChange() {
+        vmm.$data.pop = true;
+        vmm.$data.buy = false;
+        vmm.$data.success = false;
+        vmm.$data.one = true;
+        vmm.$data.two = false;
+        vmm.$data.three = false;
+        }
