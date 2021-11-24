09b1654693a7cd7d145152f07b02594737f84e20
GaliSW <gary850204@gmail.com>
2021-11-24 18:31:37 +0800

    no message

--- src/php/Manager/articleUpdate.php
+++ src/php/Manager/articleUpdate.php
@@ -19,7 +19,7 @@
         // $filePath = getFilePath().$_FILES["file"]["name"];
 
         //檔案最終存放位置
-        $filePath = $ServerRoot."/src/image/intro/".$fileName;
+        $filePath = $ServerRoot."/TFD103-G1/src/image/intro/".$fileName;
         
 
         //將暫存檔搬移到正確位置
