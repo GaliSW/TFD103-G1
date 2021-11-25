16a67463887b38df37ccf1178fbcd052da62d7d1
mark83823 <mark83823@gmail.com>
2021-11-26 01:48:51 +0800

    no message

--- src/php/product/deletemyfav.php
+++ src/php/product/deletemyfav.php
@@ -4,10 +4,10 @@
     //---------------------------------------------------
 
        //建立SQL
-       $sql = "DELETE FROM MYFAV WHERE FK_PRODUCT_ID = 2";
+    $sql = "DELETE FROM MYFAV WHERE FK_PRODUCT_ID = 2";
 
-       $statement = $pdo->prepare($sql);
-       $statement->bindValue(1 , $DELETEMYFAV);   
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(1 , $DELETEMYFAV);   
 
-       $statement->execute();
+    $statement->execute();
 ?>
